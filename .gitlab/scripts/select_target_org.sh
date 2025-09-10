9b674d40bee10fe29e1176cf3b9b3af148341f50
krishnaMurthy Donta <krishnamurthydonta@gmail.com>
2025-09-10 17:31:04 +0530

    Modify

--- .gitlab/scripts/select_target_org.sh
+++ .gitlab/scripts/select_target_org.sh
@@ -5,12 +5,15 @@ echo "Branch detected: $TARGET_BRANCH"
 case "$TARGET_BRANCH" in
   main)
     echo "ORG_ALIAS=PROD" > variables.env
+    echo "$SF_AUTH_PROD" > auth_file.json
     ;;
   qa)
     echo "ORG_ALIAS=QA" > variables.env
+    echo "$SF_AUTH_QA" > auth_file.json
     ;;
   uat)
     echo "ORG_ALIAS=UAT" > variables.env
+    echo "$SF_AUTH_UAT" > auth_file.json
     ;;
   *)
     echo "Unsupported branch: $TARGET_BRANCH"
@@ -18,4 +21,4 @@ case "$TARGET_BRANCH" in
     ;;
 esac
 
-echo "Org alias set in variables.env"
+echo "Org alias set in variables.env and auth_file.json created"
