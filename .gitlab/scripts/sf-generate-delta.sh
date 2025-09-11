6f9b0c5702007e3d06766719e258b7446be65852
krishnaMurthy Donta <krishnamurthydonta@gmail.com>
2025-09-11 18:22:53 +0530

    correct command

--- .gitlab/scripts/sf-generate-delta.sh
+++ .gitlab/scripts/sf-generate-delta.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
+
 FROM_COMMIT=$1
 TO_COMMIT=$2
 
 echo "Generating delta between $FROM_COMMIT and $TO_COMMIT..."
 
-# Ensure sfdx-git-delta is installed
-sfdx plugins --core | grep sfdx-git-delta || sfdx plugins:install sfdx-git-delta
+# Ensure sfdx-git-delta plugin is installed for sf
+sf plugins | grep sfdx-git-delta || echo y | sf plugins install sfdx-git-delta
 
-# Generate delta using sfdx instead of sf
+# Generate delta using sf CLI
 sf sgd:source:delta \
   --from "$FROM_COMMIT" \
   --to "$TO_COMMIT" \
