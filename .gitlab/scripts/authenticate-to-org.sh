6cceee5ed12425fd5a32fe3e7da6b5d164969f69
krishnaMurthy Donta <krishnamurthydonta@gmail.com>
2025-09-11 10:35:07 +0530

    authenticate-to-org

---
+++ .gitlab/scripts/authenticate-to-org.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Script to authenticate into a Salesforce org using JWT-based auth
+
+ORG_ALIAS="$1"
+AUTH_FILE="$2"
+
+echo "Authenticating into Salesforce Org: $ORG_ALIAS"
+
+# Check if sfdx/cli is available
+if ! command -v sf >/dev/null 2>&1 && ! command -v sfdx >/dev/null 2>&1; then
+  echo " Salesforce CLI (sf or sfdx) not found in the container. Exiting..."
+  exit 1
+fi
+
+# Use Salesforce CLI to authenticate
+if command -v sf >/dev/null 2>&1; then
+  sf org login sfdx-url --sfdx-url-file "$AUTH_FILE" --alias "$ORG_ALIAS" --set-default || exit 1
+else
+  sfdx auth:sfdxurl:store -f "$AUTH_FILE" -a "$ORG_ALIAS" -s || exit 1
+fi
+
+echo "Successfully authenticated into Salesforce Org: $ORG_ALIAS"
