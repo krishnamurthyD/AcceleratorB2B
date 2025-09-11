aae16616959c7dc061b3f7f612dcc6e2ec28528e
krishnaMurthy Donta <krishnamurthydonta@gmail.com>
2025-09-11 11:04:44 +0530

    Org selection from

--- .gitlab/scripts/select_target_org.sh
+++ .gitlab/scripts/select_target_org.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-TARGET_BRANCH="$1"
+BRANCH="$CI_COMMIT_REF_NAME"
 
-case "$TARGET_BRANCH" in
+case "$BRANCH" in
   main)
-    echo "ORG_ALIAS=PROD" > variables.env
+    echo "ORG_ALIAS=PROD" >> variables.env
     echo "$SF_AUTH_PROD" > auth_file.json
     ;;
   qa)
-    echo "ORG_ALIAS=QA" > variables.env
+    echo "ORG_ALIAS=QA" >> variables.env
     echo "$SF_AUTH_QA" > auth_file.json
     ;;
   uat)
-    echo "ORG_ALIAS=UAT" > variables.env
+    echo "ORG_ALIAS=UAT" >> variables.env
     echo "$SF_AUTH_UAT" > auth_file.json
     ;;
   *)
-    echo "Unsupported branch: $TARGET_BRANCH"
+    echo "Unsupported branch: $BRANCH"
     exit 1
     ;;
 esac
