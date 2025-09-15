d5977574d799486c42f707ee878f35af2fbeb571
Krishnamurthy Donta <krishnamurthy.donta@cloudodyssey.co>
2025-09-15 08:44:46 +0000

    Edit select_target_org.sh

--- .gitlab/scripts/select_target_org.sh
+++ .gitlab/scripts/select_target_org.sh
@@ -4,15 +4,15 @@ BRANCH="$CI_COMMIT_REF_NAME"
 case "$BRANCH" in
   main)
     echo "ORG_ALIAS=PROD" >> variables.env
-    echo "$SF_AUTH_PROD_B64" | base64 -d > ./auth_file.json
+    echo "$SF_AUTH_PROD" | base64 -d > ./auth_file.json
     ;;
   develop)
     echo "ORG_ALIAS=INTEGRATION" >> variables.env
-    echo "$SF_AUTH_INTEGRATION_B64" | base64 -d > ./auth_file.json
+    echo "$SF_AUTH_INTEGRATION" | base64 -d > ./auth_file.json
     ;;
   release/*)
     echo "ORG_ALIAS=UAT" >> variables.env
-    echo "$SF_AUTH_UAT_B64" | base64 -d > ./auth_file.json
+    echo "$SF_AUTH_UAT" | base64 -d > ./auth_file.json
     ;;
   *)
     echo "Unsupported branch: $BRANCH"
