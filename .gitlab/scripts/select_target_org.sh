f08b37bbc4d126f87726c98115f691def898aa90
Krishnamurthy Donta <krishnamurthy.donta@cloudodyssey.co>
2025-09-15 08:30:41 +0000

    Edit select_target_org.sh

--- .gitlab/scripts/select_target_org.sh
+++ .gitlab/scripts/select_target_org.sh
@@ -1,27 +1,4 @@
-#!/bin/sh
-# BRANCH="$CI_COMMIT_REF_NAME"
-
-# case "$BRANCH" in
-#   main)
-#     echo "ORG_ALIAS=PROD" >> variables.env
-#     echo "$SF_AUTH_PROD" > auth_file.json
-#     ;;
-#   qa)
-#     echo "ORG_ALIAS=QA" >> variables.env
-#     echo "$SF_AUTH_QA" > auth_file.json
-#     ;;
-#   uat)
-#     echo "ORG_ALIAS=UAT" >> variables.env
-#     echo "$SF_AUTH_UAT" > auth_file.json
-#     ;;
-#   *)
-#     echo "Unsupported branch: $BRANCH"
-#     exit 1
-#     ;;
-# esac
-
-
-#!/bin/sh
+#!/bin/bash
 BRANCH="$CI_COMMIT_REF_NAME"
 
 case "$BRANCH" in
