9265c836db3fff7bdfa676b4265159ca51221513
Krishnamurthy Donta <krishnamurthy.donta@cloudodyssey.co>
2025-09-15 08:23:28 +0000

    Edit select_target_org.sh

--- .gitlab/scripts/select_target_org.sh
+++ .gitlab/scripts/select_target_org.sh
@@ -27,15 +27,15 @@ BRANCH="$CI_COMMIT_REF_NAME"
 case "$BRANCH" in
   main)
     echo "ORG_ALIAS=PROD" >> variables.env
-    echo "$SF_AUTH_PROD" > auth_file.json
+    echo "$SF_AUTH_PROD" > ./auth_file.json
     ;;
   develop)
     echo "ORG_ALIAS=INTEGRATION" >> variables.env
-    echo "$SF_AUTH_INTEGRATION" > auth_file.json
+    echo "$SF_AUTH_INTEGRATION" > ./auth_file.json
     ;;
   release/*)
     echo "ORG_ALIAS=UAT" >> variables.env
-    echo "$SF_AUTH_UAT" > auth_file.json
+    echo "$SF_AUTH_UAT" > ./auth_file.json
     ;;
   *)
     echo "Unsupported branch: $BRANCH"
@@ -44,4 +44,4 @@ case "$BRANCH" in
 esac
 
 pwd
-cat auth_file.json
+cat ./auth_file.json
